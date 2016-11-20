799ea84eca4d088522d5c895dddce2362b4852bf
zigzga <447587073@qq.com>
2016-11-21 00:41:24 +0800

    base config already

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // 作为入口文件
 // import Vue from "vue";
+require("./css/index.css");
 
-document.write('It works.')
+document.write('It works. by watch');
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
         path: './build',
         publicPath: 'build/',
         filename: 'build.js'
-    }
+    },
+    module: {
+        loaders: [
+            // css loader
+            { test: /\.css$/, loader: "style-loader!css-loader"}
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin("this is build js banner")
+    ]
 }
